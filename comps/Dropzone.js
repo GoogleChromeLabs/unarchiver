6adde5a22b5d523231cb3b2263c1ac527716bbcb
Marijn Kruisselbrink <mek@chromium.org>
2019-10-21 13:27:59 -0700

    Read contents of files in archive as well

--- comps/Dropzone.js
+++ comps/Dropzone.js
@@ -5,10 +5,23 @@ import JSZip from 'jszip';
 export default function Dropzone() {
   const onDrop = useCallback(async acceptedFiles => {
     const zip = await JSZip.loadAsync(acceptedFiles[0]);
-    zip.forEach((name, file) => {
+    for (let name in zip.files) {
         console.log(name);
-        console.log(file);
-    });
+        const file = zip.files[name];
+
+        // Skip over directories
+        if (file.dir) continue;
+
+        const stream = file.nodeStream();
+        // Too bad stream is not a native stream, but instead a node stream
+        // with a totally different API...
+        stream.on('data', chunk => {
+            console.log(chunk);
+        });
+        await new Promise(resolve => {
+            stream.on('end', resolve);
+        });
+    };
   }, []);
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
 
