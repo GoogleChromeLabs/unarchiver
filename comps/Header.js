86aa92f621e4bed7f72c3b8192b12f448e32a45b
Marijn Kruisselbrink <mek@chromium.org>
2019-10-21 16:13:13 -0700

    Add origin trial token

--- comps/Header.js
+++ comps/Header.js
@@ -10,6 +10,7 @@ export default function Header() {
     <div>
       <Head>
         <link rel="manifest" href="/static/manifest.json" />
+        <meta http-equiv="origin-trial" content="AucV22yiUOQFnchsKcbTK0w6kW7pYxqgTZme6YoXqwsUT+ogAoTzzYybtcwTiya4RdmgvEIbeXJ9veB7N8TGSwkAAABTeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJmZWF0dXJlIjoiTmF0aXZlRmlsZVN5c3RlbSIsImV4cGlyeSI6MTU3NTMyODIzOX0=" />
       </Head>
       <Link href="/">
         <a style={linkStyle}>Home</a>
