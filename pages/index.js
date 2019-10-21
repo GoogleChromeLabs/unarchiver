71df70f07b5b461a0f0d6e591ef872f4e0b283a5
Chase Phillips <cmp@chromium.org>
2019-10-21 12:40:42 -0700

    Dropzone: Use example with FileReader API

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,41 @@
 // This is the Link API
 import Link from 'next/link';
-import Dropzone from 'react-dropzone';
+import {useCallback} from 'react';
+import {useDropzone} from 'react-dropzone';
+
+function MyDropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    const reader = new FileReader();
+
+    reader.onabort = () => console.log('file reading was aborted');
+    reader.onerror = () => console.log('file reading has failed');
+    reader.onload = () => {
+      // Do whatever you want with the file contents
+      const binaryStr = reader.result;
+      console.log(binaryStr);
+    };
+
+    acceptedFiles.forEach(file => reader.readAsArrayBuffer(file));
+  }, []);
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  );
+}
 
 const Index = () => (
   <div>
     <p>Unarchiver</p>
 
-    <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
-      {({getRootProps, getInputProps}) => (
-        <section>
-          <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            <p>Drag 'n' drop some files here, or click to select files</p>
-          </div>
-        </section>
-      )}
-    </Dropzone>
+    <MyDropzone/>
 
     <Link href="/about">
       <a>About</a>
