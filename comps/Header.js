e1f5f063781fa47c3cb00c061d8f66fd6f28f2f3
Adrienne Walker <enne@chromium.org>
2019-10-21 15:23:28 -0700

    point manifest to static

--- comps/Header.js
+++ comps/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
   return (
     <div>
       <Head>
-        <link rel="manifest" href="/public/manifest.json" />
+        <link rel="manifest" href="/static/manifest.json" />
       </Head>
       <Link href="/">
         <a style={linkStyle}>Home</a>
