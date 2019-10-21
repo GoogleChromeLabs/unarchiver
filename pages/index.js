67f6eacbff3707ed70bf237b47196daee0ffa428
Chase Phillips <cmp@chromium.org>
2019-10-21 12:24:51 -0700

    Add Dropzone component to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 // This is the Link API
 import Link from 'next/link';
+import Dropzone from 'react-dropzone';
 
 const Index = () => (
   <div>
     <p>Unarchiver</p>
+
+    <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+      {({getRootProps, getInputProps}) => (
+        <section>
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          </div>
+        </section>
+      )}
+    </Dropzone>
 
     <Link href="/about">
       <a>About</a>
