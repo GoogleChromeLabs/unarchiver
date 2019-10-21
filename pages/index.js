53d793c5391c51b43a828044962eb41914772e3c
Daniel Murphy <dmurph@chromium.org>
2019-10-21 11:26:26 -0700

    Initial started nextjs project

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// This is the Link API
+import Link from 'next/link';
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+    <p>Hello Next.js</p>
+  </div>
+);
+  
+export default Index;
