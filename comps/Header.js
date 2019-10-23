3ea8eb0c5ca072ccf23d88d34ab74462d65ce0d2
Chase Phillips <cmp@chromium.org>
2019-10-23 13:57:50 -0700

    OriginTrials: Add charging-penguin OT while supporting localhost OT

--- comps/Header.js
+++ comps/Header.js
@@ -5,6 +5,14 @@ const linkStyle = {
   marginRight: 15
 }
 
+const OriginTrials = [
+  // http://localhost:3000
+  "AucV22yiUOQFnchsKcbTK0w6kW7pYxqgTZme6YoXqwsUT+ogAoTzzYybtcwTiya4RdmgvEIbeXJ9veB7N8TGSwkAAABTeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJmZWF0dXJlIjoiTmF0aXZlRmlsZVN5c3RlbSIsImV4cGlyeSI6MTU3NTMyODIzOX0=",
+
+  // https://charging-penguin.appspot.com
+  "AilpfnXIrOFTRsL8OTDbxBxka0FR7IbGkvX0bujj69NMOeFfiyX/9oTysIu0oZyyYPxSTizdaY/26qvqHe6PHgMAAAB5eyJvcmlnaW4iOiJodHRwczovL2NoYXJnaW5nLXBlbmd1aW4uYXBwc3BvdC5jb206NDQzIiwiZmVhdHVyZSI6Ik5hdGl2ZUZpbGVTeXN0ZW0iLCJleHBpcnkiOjE1NzU0ODUzMTMsImlzU3ViZG9tYWluIjp0cnVlfQ==",
+];
+
 export default function Header() {
   return (
     <div>
@@ -25,7 +33,8 @@ export default function Header() {
         <meta name="msapplication-config" content="/static/favicon/browserconfig.xml" />
         <meta name="theme-color" content="#69c8a4" />
 
-        <meta httpEquiv="origin-trial" content="AucV22yiUOQFnchsKcbTK0w6kW7pYxqgTZme6YoXqwsUT+ogAoTzzYybtcwTiya4RdmgvEIbeXJ9veB7N8TGSwkAAABTeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJmZWF0dXJlIjoiTmF0aXZlRmlsZVN5c3RlbSIsImV4cGlyeSI6MTU3NTMyODIzOX0=" />
+        <meta httpEquiv="origin-trial" content={OriginTrials[0]} key="0" />
+        <meta httpEquiv="origin-trial" content={OriginTrials[1]} key="1" />
       </Head>
     </div>
   )
