ecbfba53889084ac1169a635cf8b7b69ebae9eb2
Chase Phillips <cmp@chromium.org>
2019-10-23 14:36:14 -0700

    Lighthouse: Improve score by preconnecting to storage.googleapis.com

--- comps/Header.js
+++ comps/Header.js
@@ -28,6 +28,7 @@ export default function Header() {
         <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/static/favicon/site.webmanifest" />
         <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#69c8a4" />
+        <link rel="preconnect" href="https://storage.googleapis.com" />
         <link rel="shortcut icon" href="/static/favicon/favicon.ico" />
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="msapplication-config" content="/static/favicon/browserconfig.xml" />
