a0d341955fd9e4c9250deb08902304a89205d3e0
Adrienne Walker <enne@chromium.org>
2019-10-23 17:37:05 -0700

    Feature detection

--- comps/Layout.js
+++ comps/Layout.js
@@ -7,6 +7,30 @@ const layoutStyle = {
 }
 
 export default function Layout(props) {
+  // Feature detection.
+  // For server-side rendering, window is not defined.
+  if (typeof window !== 'undefined' && !('chooseFileSystemEntries' in window)) {
+    let chromeLink = '';
+    if (window.chrome) {
+      chromeLink = (
+        <div>
+          Please enable the flag on the chrome://flags/#native-file-system-api page.
+        </div>
+      );
+    }
+    return (
+      <div style={layoutStyle}>
+        <Header />
+        <div>
+          <a href="https://wicg.github.io/native-file-system/">
+            Native File System API not supported
+          </a>
+        </div>
+        {chromeLink}
+      </div>
+    );
+  }
+
   return (
     <div style={layoutStyle}>
       <Header />
